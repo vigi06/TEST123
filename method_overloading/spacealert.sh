53272e5de800db48fe6896c34ec1fabb7ef4d2f9
vigi06 <vigneshnadar231@gmail.com>
2023-09-02 15:49:07 +0530

    this is lol

--- method_overloading/spacealert.sh
+++ method_overloading/spacealert.sh
@@ -3,7 +3,15 @@
 df -h | awk -F " " '{print $5 " " $1}' | while read output;
 
 do
+	echo "this is the thing which we want in our project"
+
+	echo "this is the story of my life it is very interesting"
+
+	echo "This is very crazy statement that we are going to discuss here"
+
 	echo "this is the space $output"
+
+	echo "this is lol which is very much fun"
 
 done
 
